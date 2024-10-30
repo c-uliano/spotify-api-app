3f34bf62c4157e66d625f064947f37e3ee61fef0
Coletta Uliano <culiano@live.com>
2024-10-30 14:14:09 +0000

    basic SearchBar built and tested

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
 import React, {useState} from "react";
 import "./App.scss";
 import Button from "./components/Button/Button";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 function App() {
-    const [click, setClick] = useState(0);
+    const [click, setClick] = useState(0); // button click count
+    const [value, setValue] = useState(''); // input value
+    const [displayed, setDisplayed] = useState(''); // displaying the input value
+    const [show, setShow] = useState(false); // to display the input value or not
 
+    // * keep an updated total of button clicks
     const handleClick = () => {
         setClick(prev => prev + 1);
     }
 
+    // * clear the button clicks
     const handleReset = () => {
         setClick(0);
+    }
+
+    // * save input value
+    const handleSave = (e:any) => {
+        const inputValue = e.target.value;
+        setValue(inputValue);
+    }
+
+    // * display input value
+    const showSave = () => {
+        setDisplayed(value);
+        setShow(true);
+        setValue('');
+    }
+
+    // * clear displayed input value
+    const clearSave = () => {
+        setDisplayed('');
     }
 
     return (
@@ -18,10 +42,22 @@ function App() {
             <h1>Spotify API App</h1>
             <p><b>The goal:</b> connect to the api, search for songs, see the results, add songs to a list, save that list as a playlist to Spotify account.</p>
             <p>I'm not concerned about styling this app. I know how to make things look good. The point is to pratice using apis and making a functional app.</p>
+
             <hr />
-            <h2>Testing the button</h2>
+
+            <h2>Testing Button</h2>
             <Button text="Button Test" onClick={handleClick} /> <Button text="Reset" onClick={handleReset} />
             <p>Clicked: {click}</p>
+
+            <h3>Testing Button with input</h3>
+            <input type="text" onChange={handleSave} value={value} />
+            <Button text="Show Value" onClick={showSave} /> <Button text="Reset" onClick={clearSave} />
+            <div>
+                {show && displayed}
+            </div>
+
+            <h2>Testing SearchBar v1</h2>
+            <SearchBar />
         </>
     );
 }
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
+import Button from "../Button/Button";
 
 const SearchBar = (props: any) => {
-    return <div>SearchBar</div>;
+    const [searchValue, setSearchValue] = useState('');
+    const [displaySearchValue, setDisplaySearchValue] = useState('');
+
+    const collectValue = (e:any) => {
+        setSearchValue(e.target.value);
+    }
+
+    const handleSearch = () => {
+        // TODO take value from input
+        // TODO search the api data for input value
+
+        setDisplaySearchValue(searchValue);
+    }
+
+    const clearSearch = () => {
+        setSearchValue('');
+        setDisplaySearchValue('');
+    }
+
+    return (
+        <>
+            <input type="text" onChange={collectValue} value={searchValue} />
+            <Button text="Search" onClick={handleSearch} /> <Button text="Reset" onClick={clearSearch} />
+            {/* remove this, just for testing */}
+            <div>{displaySearchValue}</div>
+        </>
+    );
 };
 
 export default SearchBar;
